0bf1f0bcb7eb52719727c81d6be8dec8faca1b11
knightfury16 <suhaibknight@gmail.com>
2022-11-12 12:28:54 +0600

    Product pagination, filter and search feature added.

--- backend/src/routers/product.js
+++ backend/src/routers/product.js
@@ -2,12 +2,52 @@ const prisma = require('../db/prisma');
 const express = require('express');
 const productValidationSchema = require('../utils/productValidationSchema');
 const validUpdate = require('../utils/validUpdate');
+const { Category } = require('@prisma/client');
 const router = new express.Router();
 
 // get all products from the database
+/* 
+  -Get(/api/products) -> get all the products
+  -Get(/api/products?keyword=apple) -> get all products where name contains the keywors query
+  -Get(/api/products?category=Laptop) -> get all products where category is Laptop
+  -Get(/api/products?rating=4) -> get all products where rating is greater than or equal 4
+  -Get(/api/products?price=1-100) -> get all products where price is gte 1 and lte 100
+  -Get(/api/products?page=2) -> get all products of page 2
+
+*/
 router.get('/', async (req, res) => {
-  const products = await prisma.product.findMany({});
-  res.status(200).send(products);
+  const priceRange = req.query.price ? req.query.price.split('-') : undefined;
+  const resPerPage = 5;
+  const currentPage = req.query.page || 1;
+
+  const products = await prisma.product.findMany({
+    where: {
+      name: {
+        contains: req.query.keyword ? req.query.keyword : '',
+        mode: 'insensitive'
+      },
+      category: {
+        in: req.query.category in Category ? [req.query.category] : Object.keys(Category)
+      },
+      ratings: {
+        gte: req.query.rating ? +req.query.rating : 1
+      },
+      price: priceRange
+        ? {
+            gte: +priceRange[0],
+            lte: +priceRange[1]
+          }
+        : {}
+    },
+    orderBy: { createdAt: 'desc' },
+    take: resPerPage,
+    skip: resPerPage * (currentPage - 1)
+  });
+
+  res.status(200).json({
+    count: products.length,
+    products
+  });
 });
 
 // get single product by id
