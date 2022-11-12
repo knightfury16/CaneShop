c046d68c01019be990fad78840abd8ee6d979e3d
knightfury16 <suhaibknight@gmail.com>
2022-11-12 09:38:06 +0600

    migration history got invalidated, new created.

--- backend/prisma/migrations/20221112033605_init/migration.sql
+++ backend/prisma/migrations/20221112033605_init/migration.sql
@@ -1,45 +1,45 @@
 -- CreateEnum
-CREATE TYPE "Category" AS ENUM ('ELECTRONICS', 'LAPTOP', 'CAMERAS', 'ACCESSORIES', 'HEADPHONES', 'FOOD', 'BOOKS', 'CLOTHES', 'BEAUTY', 'HEALTH', 'SPORTS', 'OUTDOORS', 'HOME');
+CREATE TYPE "Category" AS ENUM ('Electronics', 'Laptop', 'Cameras', 'Accessories', 'Headphones', 'Food', 'Books', 'Clothes', 'Beauty', 'Health', 'Sports', 'Outdoors', 'Home');
 
 -- CreateEnum
 CREATE TYPE "Role" AS ENUM ('ADMIN', 'SELLER', 'BUYER');
 
 -- CreateTable
 CREATE TABLE "Product" (
-    "id" TEXT NOT NULL,
+    "id" SERIAL NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "name" VARCHAR(100) NOT NULL,
-    "price" INTEGER NOT NULL DEFAULT 0,
+    "price" DOUBLE PRECISION DEFAULT 0,
     "description" TEXT NOT NULL,
-    "rating" DOUBLE PRECISION NOT NULL DEFAULT 0,
+    "ratings" DOUBLE PRECISION DEFAULT 0,
     "category" "Category" NOT NULL,
     "seller" TEXT NOT NULL,
-    "stock" INTEGER NOT NULL DEFAULT 0,
-    "numOfReviews" INTEGER NOT NULL DEFAULT 0,
+    "stock" INTEGER DEFAULT 0,
+    "numOfReviews" INTEGER DEFAULT 0,
 
     CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Reviews" (
-    "id" TEXT NOT NULL,
+    "id" SERIAL NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "name" TEXT NOT NULL,
-    "rating" INTEGER NOT NULL,
+    "ratings" INTEGER NOT NULL,
     "comment" TEXT NOT NULL,
-    "productId" TEXT NOT NULL,
+    "productId" INTEGER NOT NULL,
 
     CONSTRAINT "Reviews_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Images" (
-    "id" TEXT NOT NULL,
+    "id" SERIAL NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
-    "productId" TEXT NOT NULL,
+    "productId" INTEGER NOT NULL,
     "url" TEXT NOT NULL,
     "publicId" TEXT NOT NULL,
 
@@ -53,7 +53,7 @@ CREATE TABLE "User" (
     "LastName" VARCHAR(50) NOT NULL,
     "Email" TEXT NOT NULL,
     "PhoneNum" TEXT NOT NULL,
-    "DateOfBirth" TIMESTAMP(3),
+    "DateOfBirth" TEXT,
     "Password" TEXT NOT NULL,
     "Gender" TEXT NOT NULL,
     "Address" TEXT NOT NULL,
