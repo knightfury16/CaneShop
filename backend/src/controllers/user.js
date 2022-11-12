591a1e19247b0c0def1c0373e61ea609a2ec9e52
Abidstic101 <abid71@student.sust.edu>
2022-11-12 15:54:41 +0600

    Added jwt authentication , added update user api, added role verification

--- backend/src/controllers/user.js
+++ backend/src/controllers/user.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcrypt");
 const prisma = require("./../db/prisma");
+const jwt = require("jsonwebtoken");
 
 //login user by searching email in the db and then comparing password has and password in db
 const login = async (req, res) => {
@@ -16,9 +17,21 @@ const login = async (req, res) => {
             if (!isValid) {
                 res.status(400).json({ message: "Invalid password" });
             } else {
+                const token = jwt.sign(
+                    {
+                        user_info: {
+                            id: user.id,
+                            Role: user.role,
+                        },
+                    },
+                    process.env.JWT_SECRET,
+                    {
+                        expiresIn: "1h",
+                    }
+                );
                 res.status(200).json({
                     message: "User logged in successfully",
-                    user,
+                    token,
                 });
             }
         }
@@ -80,9 +93,16 @@ const register = async (req, res) => {
 };
 
 const getAllUsers = async (req, res) => {
+    const role = req.user_info.Role;
+    console.log(role);
     try {
-        const users = await prisma.user.findMany();
-        res.status(200).json(users);
+        if (role === "BUYER") {
+            const users = await prisma.user.findMany();
+            res.status(200).json(users);
+        }
+        else{
+            res.status(401).json({message:"Unauthorized"});
+        }
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
@@ -90,16 +110,9 @@ const getAllUsers = async (req, res) => {
 //Update users information by searching for the user by email
 const updateUserById = async (req, res) => {
     const { Email } = req.params;
-    const {
-        FirstName,
-        LastName,
-        DateOfBirth,
-        PhoneNum,
-        Password,
-        Address,
-        Gender,
-    } = req.body;
-    const hashedPassword = await bcrypt.hash(Password, 10);
+    const { FirstName, LastName, DateOfBirth, PhoneNum, Address, Gender } =
+        req.body;
+
     try {
         const user = await prisma.user.findFirst({ where: { Email } });
         console.log(user);
@@ -115,12 +128,14 @@ const updateUserById = async (req, res) => {
                     LastName: LastName,
                     DateOfBirth: DateOfBirth,
                     PhoneNum: PhoneNum,
-                    Password: hashedPassword,
                     Address: Address,
                     Gender: Gender,
                 },
             });
-            res.status(200).json({ message: "User updated successfully" ,updatedUser});
+            res.status(200).json({
+                message: "User updated successfully",
+                updatedUser,
+            });
         }
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -128,19 +143,65 @@ const updateUserById = async (req, res) => {
 };
 //Delete user by searching for the user by email
 const deleteUserById = async (req, res) => {
-    const { Email } = req.params;
+    const { id } = req.params;
     try {
-        const user = await prisma.user.findFirst({ where: { Email } });
+        const user = await prisma.user.findFirst({ where: { id } });
         if (!user) {
             res.status(404).json({ message: "User not found" });
         } else {
-            await prisma.user.delete({ where: { Email: Email } });
+            await prisma.user.delete({ where: { id: id } });
             res.status(200).json({ message: "User deleted successfully" });
         }
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
-}
+};
+// updating user information by searching for the user by id from token
+const updateUserInfo = async (req, res) => {
+    const id = req.user_info.id;
+    const {
+        FirstName,
+        LastName,
+        DateOfBirth,
+        PhoneNum,
+        Password,
+        Address,
+        Gender,
+        Email,
+    } = req.body;
+    const hashedPassword = await bcrypt.hash(Password, 10);
+    try {
+        const user = await prisma.user.findFirst({ where: { id: id } });
+        if (!user) {
+            res.status(404).json({ message: "User not found" });
+            return;
+        } else {
+            console.log("eije user:", user, "id:", id);
+            const updatedUser = await prisma.user.update({
+                where: {
+                    id: id,
+                },
+                data: {
+                    FirstName: FirstName,
+                    LastName: LastName,
+                    DateOfBirth: DateOfBirth,
+                    PhoneNum: PhoneNum,
+                    Address: Address,
+                    Email: Email,
+                    Password: hashedPassword,
+                    Gender: Gender,
+                },
+            });
+            console.log("helloo");
+            res.status(201).json({
+                message: "User updated successfully",
+                updatedUser,
+            });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
 
 module.exports = {
     login,
@@ -148,4 +209,5 @@ module.exports = {
     getAllUsers,
     updateUserById,
     deleteUserById,
+    updateUserInfo,
 };
---
+++ backend/src/middleware/verify_jwt.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+
+const verifyjwt= (req,res,next)=>{
+    if ( req.headers.authorization || req.headers.Authorization){
+        const token = req.headers.authorization;
+   
+        if(token)
+        {
+            jwt.verify(token,process.env.JWT_SECRET,(err,decoded)=>{
+                if(err){
+                    res.status(401).json({
+                        message:"Unauthorized"
+                    });
+                }
+                else{
+                    req.user_info=decoded.user_info;
+                    // res.status(200).json({message:"Authorized",user_info:decoded.user_info});
+                    next();
+                }
+            });
+        }
+
+    }
+};
+module.exports=verifyjwt;
