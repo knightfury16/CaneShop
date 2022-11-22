bd0c9233615facd324fe77df8cf37da861af9c27
Abidstic101 <abid71@student.sust.edu>
2022-11-22 14:45:23 +0600

    updated frontned

--- frontend/caneshop/.next/static/chunks/webpack.js
+++ frontend/caneshop/.next/static/chunks/webpack.js
@@ -107,7 +107,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c12d348552787a3b"; }
+/******/ 		__webpack_require__.h = function() { return "5a53e8eb703696b8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
